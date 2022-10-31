f07dd1a0911a7ae68aecd749313f82d3f5ff2503
Anna B <106448223+LunarMunkey@users.noreply.github.com>
2022-10-30 23:46:50 -0500

    challenge 19 update

--- script.js
+++ script.js
@@ -18,13 +18,59 @@ function toggleMenu () {
 
 hamburger.addEventListener("click", toggleMenu);
 
-const home = document.querySelector(".hero");
-const projects = document.querySelector(".projects");
-const about = document.querySelector(".about");
+function selectElementByClass(className) {
+    return document.querySelector(`.${className}`);
+  }
+
+const SECTIONS = [
+    selectElementByClass('hero'),
+    selectElementByClass('work'),
+    selectElementByClass('aboutMe'),
+];
+
+const NAV_ITEMS = {
+    hero: selectElementByClass('homeNavItem'),
+    work: selectElementByClass('projectsNavItem'),
+    aboutMe: selectElementByClass('aboutNavItem'),
+};
+
+const OBSERVER_OPTIONS = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3,
+  };
+
+  function observerCallback(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // get the nav item corresponding to the id of the section
+        // that is currently in view
+        var navItem = NAV_ITEMS[entry.target.id];
+        // add 'active' class on the navItem
+        navItem.classList.add('active');
+        // remove 'active' class from any navItem that is not
+        // same as 'navItem' defined above
+        Object.values(NAV_ITEMS).forEach((item) => {
+          if (item != navItem) {
+            item.classList.remove('active');
+          }
+        });
+      }
+    });
+  }
+  
+  const observer = new IntersectionObserver(observerCallback, OBSERVER_OPTIONS);
+  
+  SECTIONS.forEach((sec) => observer.observe(sec));
 
 
-home.addEventListener("click").scrollIntoView({behavior: 'smooth'});
-projects.scrollIntoView({behavior: 'smooth'});
+//const home = document.querySelector(".hero");
+//const projects = document.querySelector(".projects");
+//const about = document.querySelector(".about");
+
+
+//home.addEventListener("click").scrollIntoView({behavior: 'smooth'});
+//projects.scrollIntoView({behavior: 'smooth'});
 
 //const hamburger = document.getElementById('hamburger');
 //const navUL = document.getElementById('navUL');
